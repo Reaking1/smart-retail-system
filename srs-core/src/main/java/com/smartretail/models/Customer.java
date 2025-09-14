05b936cdff824932c1d16c7becf94e3918f4b49a
Reaking1 <reabetsweramoswane@gmail.com>
2025-09-14 22:15:09 +0200

    we will do some testing here and ther

--- srs-core/src/main/java/com/smartretail/models/Customer.java
+++ srs-core/src/main/java/com/smartretail/models/Customer.java
@@ -3,25 +3,22 @@ package com.smartretail.models;
 import jakarta.persistence.*;
 import java.util.List;
 
-
 @Entity
 @Table(name = "customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
 
- @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
- private Long customerId;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
 
- private String name;
- private String email;
- private String phone;
- private String address;
+    // One customer can have many orders
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Order> orders;
 
- //One customer can have many orders
- @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
- private List<Order> orders;
-
-
-  // Getters and setters
+    // Getters and setters
     // ...
 }
